80b2815ade89f36c715a1162df755d25a59aa47f
Augusto Ficara <134169439+AugustoFicara@users.noreply.github.com>
2023-10-17 11:58:16 -0300

    Add files via upload

--- Front-End/js/scriptLogin.js
+++ Front-End/js/scriptLogin.js
@@ -1,8 +1,7 @@
-
 const btnIniciarConCuenta = document.getElementById("iniciarSesionDatos"),
-    btnRegistrarDatos = document.getElementById("registrarDatos"),
-    formRegistrar = document.querySelector(".registrar"),
-    formIniciar = document.querySelector(".iniciar");
+      btnRegistrarDatos = document.getElementById("registrarDatos"),
+      formRegistrar = document.querySelector(".registrar"),
+      formIniciar = document.querySelector(".iniciar");
 
 btnRegistrarDatos.addEventListener("click", e => {
     formIniciar.classList.add("ocultar");
@@ -14,77 +13,3 @@ btnIniciarConCuenta.addEventListener("click", e => {
     formIniciar.classList.remove("ocultar")
 })
 
-function cargarUsuario() {
-    const nombreInput = document.getElementById("nombreRegistracion");
-    const emailInput = document.getElementById("emailRegistracion");
-    const contraseñaInput = document.getElementById("contraseñaRegistracion");
-
-    const nombre = nombreInput.value.trim();
-    const nombreSplit = nombre.split(" ");
-    let datosCliente = {
-        nombre: nombreSplit[0],
-        apellido: nombreSplit[1],
-        email: emailInput.value,
-        contraseña: contraseñaInput.value
-    };
-
-    // Validaciones aca
-    console.log(JSON.stringify(datosCliente));
-
-    fetch('http://localhost:8080/cliente', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(datosCliente),
-      })      
-        .then(response => {
-            if (!response.ok) {
-                //MOSTRAR CARTEL DE QUE HUBO ALGUN ERROR
-                throw new Error('Usuario existente');
-            }
-            //ACA TENDRIAMOS QUE HACER UN CARTEL O ALGO DE INICIO EXITOSO O DE BIENVENIDA
-            window.location.href = 'mainMenu.html';
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-function iniciarSesionUsuario() {
-    const emailInput = document.getElementById("emailLogin");
-    const contraseñaInput = document.getElementById("contraseñaLogin");
-
-    let datosCliente = {
-        email: emailInput.value,
-        contraseña: contraseñaInput.value
-    };
-
-    // Validaciones aca
-    console.log(JSON.stringify(datosCliente));
-
-    fetch('http://localhost:8080/cliente/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(datosCliente),
-      })      
-        .then(response => {
-            if (!response.ok) {
-                //MOSTRAR CARTEL DE QUE HUBO ALGUN ERROR
-                throw new Error('Usuario existente');
-            }
-            //ACA TENDRIAMOS QUE HACER UN CARTEL O ALGO DE INICIO EXITOSO O DE BIENVENIDA
-            window.location.href = 'mainMenu.html';
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
